7ebbb50d73b633fb67ced6609a5155e522480fe2
Adiletkozha <adilet-kozha.suleimen@sdu.edu.kz>
2015-11-23 01:40:33 +0600

    Navigation controller included page jumping

---
+++ AJMapKit/AJMapKit/RegisterView.swift
@@ -0,0 +1,24 @@
+//
+//  RegisterView.swift
+//  AJMapKit
+//
+//  Created by Suleymen Adilet on 11/23/15.
+//  Copyright © 2015 Alejandro Jaque. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+import GoogleMaps
+
+
+class RegisterView: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+
+   
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
